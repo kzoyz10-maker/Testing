1a95b173ebc0f9a87b3957332084e0619bd97323
kzoyz10-maker <kzoyz10@gmail.com>
2026-03-03 09:16:51 +0700

    Update Discord.lua

--- Discord.lua
+++ Discord.lua
@@ -1,1 +1,26 @@
+local Tab, Window, WindUI = ...
+if type(Tab) ~= "table" then return end
 
+local InviteCode = "y56q8zuj2r" -- GANTI INI AJA
+
+Tab:Section({ Title = "Join our Discord Server!", TextSize = 20 })
+Tab:Paragraph({
+    Title = "Kzoyz Community",
+    Desc = "Mari bergabung dengan komunitas kami untuk mendapatkan update terbaru, diskusi, dan report bug!",
+    Image = "messages-square",
+    ImageSize = 48,
+    Buttons = {
+        {
+            Title = "Copy Link Discord",
+            Icon = "link",
+            Callback = function()
+                if setclipboard then 
+                    setclipboard("https://discord.gg/" .. InviteCode)
+                    if WindUI then 
+                        WindUI:Notify({ Title = "Discord", Content = "Link berhasil disalin ke Clipboard!", Icon = "check" }) 
+                    end
+                end
+            end
+        }
+    }
+})
